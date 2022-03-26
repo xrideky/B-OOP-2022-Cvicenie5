7dec36960fa24e86aab5a3313fb9cc8f859a84f3
xrideky <xrideky@stuba.sk>
2022-03-26 18:19:01 +0100

    streamy

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -6,18 +6,42 @@ import sk.stuba.fei.uim.oop.zvierata.Zviera;
 import sk.stuba.fei.uim.oop.zvierata.ZvieraComparator;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         List<Zviera> zvierata = new ArrayList<>(List.of(new Pes("ducno"),new Macka("murko"),
                 new Pes("luna"),new Macka("garfield")));
 
-        zvierata.sort(new ZvieraComparator());
-        System.out.println(zvierata);
+        //streamy su kolekcie na ktore sa vieme ako keby pripojit a vieme na nich pocuvat
+        // a v priebehu casu tam vedia bezat nejake udaje toho typu ktoreho typu je ten stream
 
-        zvierata.sort((o1, o2) -> -o1.getMeno().compareTo(o2.getMeno()));  //- pred o1 je zoradenie zozadu
-        System.out.println(zvierata);
-        zvierata.sort(Comparator.comparing(Zviera::getMeno));
-        System.out.println(zvierata);
+        zvierata.stream().forEach(System.out::println); //list zvierat sa mi zmenil na stream ktory obsahuje zvierata
+
+        List<Zviera> zvierata2 = zvierata.stream().collect(Collectors.toList());
+
+        zvierata2 = zvierata2.stream()
+                .sorted((o1, o2) -> o1.getMeno().compareTo(o2.getMeno()))
+                .collect(Collectors.toList());
+
+        System.out.println("-----");
+        zvierata2.stream().forEach(System.out::println);
+
+        String[] mena = zvierata2.stream()
+                .map(zviera -> zviera.getMeno())
+                .toArray(size ->new String[size]);
+
+        System.out.println("-----");
+        Stream.of(mena).forEach(System.out::println);
+
+        List<Pes> psi = zvierata.stream()
+                .filter(zviera -> zviera instanceof Pes)
+                .map(pes -> (Pes) pes)
+                .collect(Collectors.toList());
+
+        System.out.println("-----");
+        psi.forEach(System.out::println);
+
     }
 }
