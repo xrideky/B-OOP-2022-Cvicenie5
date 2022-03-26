989caf37dc182c574bd91c4ecd828626617ad4e2
xrideky <xrideky@stuba.sk>
2022-03-26 17:42:28 +0100

    lambda (anonymne funkcie)

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,35 +1,22 @@
 package sk.stuba.fei.uim.oop;
 
-
-import sk.stuba.fei.uim.oop.zvierata.Macka;
-import sk.stuba.fei.uim.oop.zvierata.Pes;
-import sk.stuba.fei.uim.oop.zvierata.Zviera;
-
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Zviera> zvierata2 = new ArrayList<>(List.of(new Pes("dunco"), new Macka("garfield")));
-
-        Set<String> auta = new HashSet<>();
+        List<String> auta = new ArrayList<>();
         auta.add("Volvo");
         auta.add("Audi");
         auta.add("BMW");
         auta.add("Fiat");
-        auta.add("Volvo"); //neopakuje sa
 
-        System.out.println(auta);
-        System.out.println(auta.size());
+        auta.forEach(s -> System.out.println(s)); //ctrl+space doplnenie lambdy
 
-        if(auta.contains("Volvo")){
-            auta.remove("Volvo");
-        }
-
-        List<String> auta2 = new ArrayList<>(List.of("Volvo","BMW"));
-        System.out.println(auta);
-
-        auta.removeAll(auta2);
-        System.out.println(auta);
+        auta.forEach(s -> {
+            if(Objects.equals(s, "BMW")){
+                System.out.println("Auto ktore nema smerovky (:");
+            }
+        });
 
     }
 }
